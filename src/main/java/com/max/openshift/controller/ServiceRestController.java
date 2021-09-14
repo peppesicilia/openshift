2c0c78d409a95fd9bc55c2cbce81d04f5661b7db
peppesicilia <peppe.zocco93@gmail.com>
2021-09-14 15:22:32 +0200

    First commit

---
+++ src/main/java/com/max/openshift/controller/ServiceRestController.java
@@ -0,0 +1,23 @@
+package com.max.openshift.controller;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.max.openshift.model.Contatto;
+
+@RestController
+public class ServiceRestController {
+	@RequestMapping("test")
+	public List<Contatto> serviceTest() {
+		List<Contatto>lista = new ArrayList<Contatto>();
+		lista.add(new Contatto("Mario","Rossi","mario"));
+		lista.add(new Contatto("Anna","Verdi","anna"));
+		lista.add(new Contatto("Paolo","Neri","paolo"));
+		lista.add(new Contatto("Maria","Arancio","maria"));
+		return lista;
+	}
+}
